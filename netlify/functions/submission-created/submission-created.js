1f3758f272f20667d8c88ed2cb23032a142ab865
dalton05 <dalton05@gmail.com>
2022-12-04 17:53:52 -0700

    just messing around at this point

--- netlify/functions/submission-created/submission-created.js
+++ netlify/functions/submission-created/submission-created.js
@@ -5,26 +5,44 @@
 const fetch = require('node-fetch')
 const { EMAIL_AUTH } = process.env
 
-
-
 exports.handler = async event => {
-  const message = JSON.parse(event.body).payload
-  console.log(`Recieved a submission: ${message}`)
-  return fetch('https://rest.sendinc.com/message.json', {
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${EMAIL_AUTH}`,
-      email: 'dalton05@gmail.com',
-      recipients: 'mitchelljdalton@gmail.com',
-      subject: 'First sendinc message',
-      message: 'Here is the thing posted to website',
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(`Submitted to SendInc:\n ${data}`)
+  try {
+    const accountInfo = await fetch('https://rest.sendinc.com/account.json', {
+      method: 'POST', 
+      headers: {
+        Authorization: `Basic ${EMAIL_AUTH}`,
+      }
     })
-    .catch(error => ({ statusCode: 422, body: String(error) }))
+    console.log(event.json())
+    console.log(EMAIL_AUTH)
+    console.log(await accountInfo.json())
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+
+
+  // const message = JSON.parse(event.body).payload
+  // console.log(`Recieved a submission: ${message}`)
+
+  
+
+  // return fetch('https://rest.sendinc.com/message.json', {
+  //   method: 'POST',
+  //   headers: {
+  //     Authorization: `Basic ${EMAIL_AUTH}`,
+  //     email: 'dalton05@gmail.com',
+  //     recipients: 'mitchelljdalton@gmail.com',
+  //     subject: 'First sendinc message',
+  //     message: 'Here is the thing posted to website',
+  //   }
+  // })
+  //   .then(response => console.log(response.json()))
+  //   .then(data => {
+  //     console.log(`Submitted to SendInc:\n ${data}`)
+  //   })
+  //   .catch(error => ({ statusCode: 422, body: String(error) }))
+
 
 
