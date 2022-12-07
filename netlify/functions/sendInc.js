b33d489431b96e873713909edea5c03904a10f97
dalton05 <dalton05@gmail.com>
2022-12-06 18:16:26 -0700

    configure functions

--- netlify/functions/sendInc.js
+++ netlify/functions/sendInc.js
@@ -1,19 +1,20 @@
 import fetch from 'node-fetch'
 
 export const handler = async (event, context) => {
-  const AUTH_CREDENTIALS = process.env
-  const ACCOUNT_EMAIL = process.env
-
+  const {AUTH_CREDENTIALS, ACCOUNT_EMAIL} = process.env
   const SENDINC_API = `https://rest.sendinc.com/message.json?email=${ACCOUNT_EMAIL}&recipients=${ACCOUNT_EMAIL}`
-  const opts = {
-      method: 'POST', 
+  
+  console.log(SENDINC_API, opts)
+  
+  const response = await fetch(SENDINC_API, {
+    method: 'POST', 
       headers: {
         'Authorization': `Basic ${Buffer.from(AUTH_CREDENTIALS).toString('base64')}`
       }
-    }
-  console.log(SENDINC_API, opts)
-  const response = await fetch(SENDINC_API, opts)
+  })
+  
   console.log(response) 
+  
   return {
     statuscode: 200,
     body: response ? response : {}
