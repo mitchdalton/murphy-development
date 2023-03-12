da6c984ccc37e9607c8764b8dd5071e00e4226c7
dalton05 <dalton05@gmail.com>
2023-03-12 17:04:38 -0600

    n

--- netlify/functions/sendInc.js
+++ netlify/functions/sendInc.js
@@ -4,21 +4,18 @@ import {parse} from 'querystring'
 
 export const handler = async (event, context) => {
   const {AUTH_CREDENTIALS, ACCOUNT_EMAIL} = process.env
-  console.log('EVENT.QUERYSTRINPARAMS STUPID THING!!!!!********!!!!!*******!!!!!!', event.queryStringParameters)
   let body = {}
+  
   try {
     body = JSON.parse(event.body)
   } catch (e) {
     body = parse(event.body)
   }
 
-  console.log('CONSOOOLLLEEE LOGGGING BODDDDYYYYYYYYYYYYYYYYYY&*&^&(@&*^#&@#%$%@^&*#^))@^*@#%^(@#%^&($@^&*#$',body)
+  const subject = `Patient referral from ${body.doctor}))`
+  const message = `${body.doctor} has sent a referral for ${body.patient}`
 
-  // const name = event.queryStringParameters.name
-  // const subject = 'Patient Referral from *doc name*
-  // const message = '*doc name* has sent a referral form for *patient name*'
-
-  const SENDINC_API = `https://rest.sendinc.com/message.json?email=${ACCOUNT_EMAIL}&recipients=dalton05@gmail.com&subject=hello&message=dafuq-`  
+  const SENDINC_API = `https://rest.sendinc.com/message.json?email=${ACCOUNT_EMAIL}&recipients=dalton05@gmail.com&subject=${subject}&message=${message}`  
 
   const response = await fetch(SENDINC_API, {
     method: 'POST', 
