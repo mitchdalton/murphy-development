84ab519f6bba7645871c0817f6c56cc9a5871383
dalton05 <dalton05@gmail.com>
2023-04-07 16:06:21 -0600

    updat ui

--- netlify/functions/sendInc.js
+++ netlify/functions/sendInc.js
@@ -12,19 +12,24 @@ export const handler = async (event, context) => {
     body = parse(event.body)
   }
 
-  const subject = `Patient referral from ${body.doctor}))`
+  const subject = `Patient referral from ${body.doctor}`
   const message = `${body.doctor} has sent a referral for ${body.patient}`
 
   const SENDINC_API = `https://rest.sendinc.com/message.json?email=${ACCOUNT_EMAIL}&recipients=dalton05@gmail.com&subject=${subject}&message=${message}`  
 
   const response = await fetch(SENDINC_API, {
     method: 'POST', 
-      headers: {
-        'Authorization': `Basic ${Buffer.from(AUTH_CREDENTIALS).toString('base64')}`
-      }
+    headers: {
+      'Authorization': `Basic ${Buffer.from(AUTH_CREDENTIALS).toString('base64')}`
+    }
   })
   
-  console.log(response) 
+  if (response) {
+    return {
+      statuscode: 200,
+      body: JSON.stringify({})
+    }
+  } 
   
   return {
     statuscode: 200,
